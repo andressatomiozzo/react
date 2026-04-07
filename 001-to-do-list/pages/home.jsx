701e4b1c8f6ec2946fd665f18e50bf615c027d60
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-04-07 16:41:16 -0400

    001 - To-do list - First steps - I styled the project.

--- 001-to-do-list/pages/home.jsx
+++ 001-to-do-list/pages/home.jsx
@@ -7,32 +7,31 @@ const Home = () => {
 
   const addTask = () => {
     if (inputRef.current.value === "" || inputRef.current.value === " ") {
-      console.log("Insira um valor válido!");
+      console.log("Insira uma tarefa!");
     } else if (!tasks.includes(inputRef.current.value)) {
       setTasks([...tasks, inputRef.current.value]);
       inputRef.current.value = "";
     } else {
-      console.log("Ops, este valor já tem no array!");
+      console.log("Ops, esta tarefa já tem no array!");
     }
   };
 
   const deleteTask = (taskDeleted) => {
-    console.log(taskDeleted);
     setTasks(tasks.filter((task) => task !== taskDeleted));
   };
 
   return (
     <div>
       <h1>Lista de tarefas</h1>
-      <div>
-        <input type="text" placeholder="tarefa..." ref={inputRef} />
+      <div className="addTask">
+        <input type="text" placeholder="Tarefa..." ref={inputRef} />
         <button onClick={addTask}>Adicionar</button>
       </div>
 
       {tasks.map((task) => (
-        <div key={task}>
+        <div key={task} className="task-toDo">
           <p>{task}</p>
-          <button onClick={() => deleteTask(task)}>🗑️</button>
+          <button onClick={() => deleteTask(task)}>✅</button>
         </div>
       ))}
     </div>
