078221f5e65b74c3969c55aa987d78961a1f5293
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-04-08 18:08:57 -0400

    002 - User registration 1 - stylized UserCard

--- 002-user-registration-1/src/components/UserCard.jsx
+++ 002-user-registration-1/src/components/UserCard.jsx
@@ -5,9 +5,9 @@ const UserCard = ({ user }) => {
     <div className="user-card">
       <img className="user-card-avatar" src={`https://robohash.org/${user.id}`} alt="" />
       <div className="user-card-info">
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-          <p>{user.age}</p>
+        <p>Nome: {user.name}</p>
+        <p>Email: {user.email}</p>
+        <p>Idade: {user.age}</p>
       </div>
     </div>
   );
