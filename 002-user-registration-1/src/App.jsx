a83822de48484f16546ed413197328a1f0dad6ee
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-04-09 10:10:49 -0400

    001 and 002 - changes in how to retrieve the input value

--- 002-user-registration-1/src/App.jsx
+++ 002-user-registration-1/src/App.jsx
@@ -1,52 +1,65 @@
 import { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
-import UserCard from './components/UserCard'
+import { v4 as uuidv4 } from "uuid";
+import UserCard from "./components/UserCard";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [age, setAge] = useState("");
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    age: "",
+  });
+
   const [users, setUsers] = useState([]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const newUser = {
       id: uuidv4(),
-      name,
-      email,
-      age
-    }
+      ...form,
+    };
 
-    setUsers([...users, newUser])
+    setUsers((prev) => [...prev, newUser]);
 
-    
+    setForm({
+      name: "",
+      email: "",
+      age: "",
+    });
   };
 
   return (
     <div className="app">
       <h1>Cadastro de usuários</h1>
 
-
       <form onSubmit={handleSubmit}>
         <label htmlFor="name" className="hidden">
           Nome:
         </label>
-        <input type="text" id="name" placeholder="Nome" /* required */ value={name} onChange={(event) => setName(event.target.value)} />
+        <input type="text" id="name" name="name" placeholder="Nome" required value={form.name} onChange={handleChange} />
         <label htmlFor="email" className="hidden">
           E-mail:
         </label>
-        <input type="email" id="email" placeholder="E-mail" /* required */ value={email} onChange={(event) => setEmail(event.target.value)}/>
+        <input type="email" id="email" name="email" placeholder="E-mail" required value={form.email} onChange={handleChange} />
         <label htmlFor="age" className="hidden">
           Idade:
         </label>
-        <input type="number" id="age" placeholder="Idade" /* required */ value={age} onChange={(event) => setAge(event.target.value)}/>
-        <button type="sumbit">Cadastrar</button>
+        <input type="number" id="age" name="age" placeholder="Idade" required value={form.age} onChange={handleChange} />
+        <button type="submit">Cadastrar</button>
       </form>
       <div className="user-list">
         {users.map((user) => (
-          <UserCard key={user.id} user={user}/>
+          <UserCard key={user.id} user={user} />
         ))}
       </div>
     </div>
