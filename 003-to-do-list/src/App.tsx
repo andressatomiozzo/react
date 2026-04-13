811ceb4d2351d0b2d8206033591e72fde6cf700b
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-04-13 17:52:25 -0400

    003 - To-do List - I added the functions to complete and delete a task.

--- 003-to-do-list/src/App.tsx
+++ 003-to-do-list/src/App.tsx
@@ -5,27 +5,42 @@ import { useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([
-    { id: 1,
-      title: "Estudar React",
-      description: "Estudar os conceitos básicos de React, como componentes, props e estado.",
-      completed: false,
-    },
-    { id: 2,
+    { id: 1, title: "Estudar React", description: "Estudar os conceitos básicos de React, como componentes, props e estado.", completed: false },
+    {
+      id: 2,
       title: "Criar um projeto",
       description: "Criar um projeto simples usando React para praticar os conceitos aprendidos.",
       completed: false,
     },
-    { id: 3,
+    {
+      id: 3,
       title: "Aprender sobre hooks",
       description: "Estudar os hooks do React, como useState e useEffect, para gerenciar estado e efeitos colaterais.",
       completed: false,
     },
   ]);
+
+  function onTaskClick(taskId: number) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, completed: !task.completed };
+      }
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onTaskDelete(taskId: number) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  }
+
   return (
     <div className="content">
       <h1>Gerenciador de Tarefas</h1>
       <AddTask />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onTaskClick={onTaskClick} onTaskDelete={onTaskDelete} />
     </div>
   );
 }
--- 003-to-do-list/src/components/Tasks/index.tsx
+++ 003-to-do-list/src/components/Tasks/index.tsx
@@ -1,14 +1,25 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
 import "./style.css";
 
 interface props {
   tasks: { id: number; title: string; description: string; completed: boolean }[];
+  onTaskClick: (taskId: number) => void;
+  onTaskDelete: (taskId: number) => void;
 }
 
-function Tasks(props: props) {
+function Tasks({tasks, onTaskClick, onTaskDelete}: props) {
   return (
     <ul>
-      {props.tasks.map((task) => (
-        <li key={task.id}>{task.title}</li>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <button onClick={() => onTaskClick(task.id)}>{task.title}</button>
+          <button>
+            <ChevronRightIcon />
+          </button>
+          <button onClick={() => onTaskDelete(task.id)}>
+            <TrashIcon />
+          </button>
+        </li>
       ))}
     </ul>
   );
