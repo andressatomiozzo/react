c020ef315396ef905a5c7812a231c0c363858083
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-05-04 09:49:54 -0400

    testes - 07 - urando Custom Hooks - useLocalStorage

--- testes/01/src/main.jsx
+++ testes/01/src/main.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
+import "./index.css";
 import App01 from "./pages/01/App01.jsx";
 import App02 from "./pages/02/App02.jsx";
 import App03 from "./pages/03/App03.jsx";
-import "./index.css";
 import App04 from "./pages/04/App04.jsx";
 import App05 from "./pages/05/App05.jsx";
 import App06 from "./pages/06/App06.jsx";
-
+import App07 from "./pages/07/App07.jsx";
 
 createRoot(document.getElementById("root")).render(
   <>
@@ -15,6 +15,7 @@ createRoot(document.getElementById("root")).render(
     {/* <App03/>*/}
     {/* <App04 /> */}
     {/* <App05 /> */}
-    <App06 />
+    {/* <App06 /> */}
+    <App07 />
   </>,
 );
---
+++ testes/01/src/pages/07/App07.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const App07 = () => {
+  const [produto, setProduto] = useLocalStorage("produto", "");
+
+  const handleClick = ({ target }) => {
+    setProduto(target.innerText);
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
+    </div>
+  );
+};
+
+export default App07;
