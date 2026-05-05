4d48d8b61f80c117baa08073aa7e5f48afb3bdab
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-05-05 16:06:47 -0400

    Exercícios - 101 - children e props - Card de produto

--- exercises/src/pages/101-card-de-produto/App101.jsx
+++ exercises/src/pages/101-card-de-produto/App101.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import ProdutoCard from "./ProdutoCard";
 
+const App101 = () => {
+  return (
+    <>
+      <h1>Produtos da loja:</h1>
+      <ProdutoCard nome="Notebook" preco={3500} disponivel={true} />
+      <ProdutoCard nome="Celular" preco={1500} disponivel={false} />
+      <ProdutoCard nome="Tablet" preco={2000} disponivel={true} />
+    </>
+  );
+};
+
+export default App101;
--- exercises/src/pages/101-card-de-produto/ProdutoCard.jsx
+++ exercises/src/pages/101-card-de-produto/ProdutoCard.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+const ProdutoCard = ({ nome, preco, disponivel }) => {
+  return (
+    <div>
+      <h3>{nome}</h3>
+      <ul>
+        <li>R$ {preco.toFixed(2)}</li>
+        <li>{disponivel ? "Disponível" : "Indisponível"}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProdutoCard;
