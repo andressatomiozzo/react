6548209f79289a3bcbacf27b2e2a21486f3dbda6
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-05-05 11:08:34 -0400

    testes - 11 - formulários - input ripo select como componente

--- testes/01/src/pages/11/App11.jsx
+++ testes/01/src/pages/11/App11.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Input from "./form/Input";
+import Select from "./form/Select";
 
 const App11 = () => {
   const [nome, setNome] = React.useState("");
   const [email, setEmail] = React.useState("");
+  const [produtos, setProdutos] = React.useState("");
 
   return (
     <form>
       <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
       <Input id="email" label="Email" value={email} setValue={setEmail} />
+      <Select options={["notebook", "tablet"]} value={produtos} setValue={setProdutos}/>
       <button>Enviar</button>
     </form>
   );
---
+++ testes/01/src/pages/11/form/Select.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Select = ({ options, value, setValue, ...props }) => {
+  return (
+    <select value={value} onChange={({target}) => setValue(target.value)} {...props}>
+      <option value="" disabled>Selecione</option>
+      {options.map((opt) => (
+        <option key={opt} value={opt}>
+          {opt}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default Select;
