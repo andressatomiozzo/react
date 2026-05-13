a0445e42cc913b8d1a88913b0c342829428d0116
Andressa Tomiozzo <andressatomiozzo9@gmail.com>
2026-05-13 10:22:43 -0400

    004 - Página de produtos - fiz o fetch na página Home para aparecer todos os produtos

--- 004-produtos/src/Header.jsx
+++ 004-produtos/src/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="contato">Contato</Link>
+      <NavLink to="/">Home</NavLink>  {" | "}
+      <NavLink to="contato">Contato</NavLink>
     </nav>
   )
 }
--- 004-produtos/src/pages/Home.jsx
+++ 004-produtos/src/pages/Home.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [dados, setDados] = React.useState([]);
+
+  const buscar = async () => {
+    try {
+      const resposta = await fetch(`https://ranekapi.origamid.dev/json/api/produto/`);
+      const json = await resposta.json();
+      if (!resposta.ok) return err;
+      setDados(json);
+    } catch (err) {
+      console.log("Ops, algo deu errado");
+    }
+  };
+
+  React.useEffect(() => {
+    buscar();
+  }, []);
+
   return (
     <div>
-      home
-      <Link to="produtos/notebook">Notebook</Link>
-      <Link to="produtos/smartphone">Smartphone</Link>
+      {dados &&
+        dados.map((produto) => (
+          <Link to={`produtos/${produto.id}`} key={produto.id}>
+            <img src={produto.fotos[0].src} alt={`Imagem de um ${produto.nome}`} />
+            <p>{produto.nome}</p>
+          </Link>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
